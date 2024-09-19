80fae630a9a95b8fff6b0e4a4f8b90a3510a7ec1
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2024-09-19 16:21:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 console.log("Hello, World");
 function greet(){
     console.log("Good Morning");
-     console.log("Good Morning");
+    console.log("Good Morning");
 }
 
 const sum = (a,b)=>{
+    a = 4;
     return a+b;
 }
 const name = "Abhi"
